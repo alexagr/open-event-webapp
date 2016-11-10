466a92fcd340c1ee09c9188756c2817dc93b9642
Alex Agranov <alex.agranov@gmail.com>
2016-11-11 00:04:53 +0200

    automated cli mode

---
+++ cli.js
@@ -0,0 +1,24 @@
+"use strict";
+var fs = require('fs');
+var io = require('socket.io-client');
+const config = require('./config.json');
+var request = require('request').defaults({'proxy': config.proxy});
+
+var client = io.connect('http://127.0.0.1:5000');
+var i = 1;
+client.on('connect', function () {
+
+    if (i == 1) {
+        i = 0;
+        var data = {
+            name: 'limmud2016',
+            theme: 'light',
+            datasource: 'jsonupload',
+            assetmode: 'download',
+            apiendpoint: ''
+        }
+        client.emit('live', data);
+    }
+    client.close();
+});
+
---
+++ cli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+cd ../open-event-content
+./convert2.py
+cd ../open-event-webapp
+cp ../open-event-content/limmud2016.zip uploads/upload.zip
+GH_TIMEOUT="${GH_TIMEOUT:-15s}"
+node cli.js & timeout $GH_TIMEOUT npm run start;
